a4ef69ac9c98811ebb3c70e76ee8aae4f35a0624
pranav7 <developer.pranav@gmail.com>
2014-01-26 04:20:53 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Earlymorningpost::Application.config.secret_key_base = 'b1f7349e580048fd565fe364321aaaf5260a44622caedfbc303a44544de8a0c6ea50d56ab9ba1a3a892925c4ced9cfca0868d37121ee59ffd2451a450c57bb74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Earlymorningpost::Application.config.session_store :cookie_store, key: '_earlymorningpost_session'

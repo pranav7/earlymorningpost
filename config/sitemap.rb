f14672e93acce1ca197d35273320f32df8c73ee4
pranav7 <developer.pranav@gmail.com>
2014-03-22 02:32:41 +0530

    testing sitemap on amazon s3

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,9 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.earlymorningpost.com"
+SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.

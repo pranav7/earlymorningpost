6da3607a194ea832e24df62955462e386a49f858
Pranav Singh <pranav@bizongo.in>
2016-04-16 20:50:28 +0530

    Updating template from erb to haml

--- Gemfile
+++ Gemfile
@@ -69,6 +69,8 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'spork'
+
+  gem "haml-rails"
 end
 
 group :test do
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,35 @@
+class News < ActiveRecord::Base
+	attr_accessible :title, 
+					:content, 
+					:category_id, 
+					:sub_heading,
+					:author,
+					:image_courtesy, 
+					:image,
+					:remote_image_url,
+					:video_url
+
+	paginates_per 20
+
+	belongs_to :category
+
+	# Validations, a news should have a Content, Title and Category ID.
+	validates :content, presence: true
+	validates :title, presence: true
+	validates :category_id, presence: true
+
+	# to associate carrierwave images to the model
+	mount_uploader :image, ImageUploader
+
+	# This sets the order of the news from New to Old, 
+	# based on when it was updated.
+	default_scope -> { order('updated_at DESC') }
+
+	def to_param
+		"#{id}+#{title.parameterize}"
+	end
+end
+
 # == Schema Information
 #
 # Table name: news
@@ -13,36 +45,3 @@
 #  author         :string(255)
 #  image_courtesy :string(255)
 #  video_url      :string(255)
-#
-
-class News < ActiveRecord::Base
-	attr_accessible :title, 
-					:content, 
-					:category_id, 
-					:sub_heading,
-					:author,
-					:image_courtesy, 
-					:image,
-					:remote_image_url,
-					:video_url
-
-	paginates_per 20
-	belongs_to :category
-
-	# Validations, a news should have a Content, Title and Category ID.
-	validates :content, presence: true
-	validates :title, presence: true
-	validates :category_id, presence: true
-
-	# to associate carrierwave images to the model
-	mount_uploader :image, ImageUploader
-
-	# This sets the order of the news from New to Old, 
-	# based on when it was updated.
-	default_scope -> { order('updated_at DESC') }
-
-
-	def to_param
-		"#{id}+#{title.parameterize}"
-	end
-end
